be89fff4fbf4ad21d84eb54c5df7bc604d6368d0
Your Name <you@example.com>
2022-01-29 18:39:04 +0530

    Back to Start

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Learn React.js At Your Fingertips On Your Device</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Learn React.js</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
