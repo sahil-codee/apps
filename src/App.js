c3d7a0f7c95a5d26d7cd630fa629ba7c7818d888
Your Name <you@example.com>
2022-01-29 18:23:01 +0530

    Change Header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Learn React.js at your fingertips</h1>
+        <h1>Learn React.js At Your Fingertips</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
