8dc9f11274da060127e7fe3773f2e51ccd12ef0f
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-04-29 22:45:16 +0530

    Change UI

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Learn React.js</h1>
-        <Para />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello World!
       </header>
     </div>
   );
